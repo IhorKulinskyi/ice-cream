32e8dd3491b936bc569c7c41237a0d8983fe7e9a
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2022-10-13 16:19:04 +0200

    video

--- src/js/modal-more.js
+++ src/js/modal-more.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open-video]"),
     closeModalBtn: document.querySelector("[data-modal-close-video]"),
-    modal: document.querySelector("[data-location]"),
+    modal: document.querySelector("[data-modal-video]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-video");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
