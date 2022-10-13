b5f1975bd24ace15afeb0385cb85cf8bf8b1e6e4
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-10-13 17:08:53 +0500

    fixing my swiper library
    пофиксила немного свои файлы, поключила через ссылку, выкинула все лишнее из паршелов

--- src/js/by-now-modal.js
+++ src/js/by-now-modal.js
@@ -1,16 +1,16 @@
 (() => {
-	const refs = {
-	  openModalBtn: document.querySelector("[data-buy-now-modal-open-tabl]"),
-	  closeModalBtn: document.querySelector("[data-modal-close]"),
-	  modal: document.querySelector("[data-modal-buy-now]"),
-	  bodyNoScroll: document.querySelector("[data-no-scroll]"),
-	};
- 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
- 
-	function toggleModal() {
-	  refs.modal.classList.toggle("is-hidden");
-	  refs.bodyNoScroll.classList.toggle('no-scroll');
-	}
- })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-buy-now-modal-open-tabl]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal-buy-now]'),
+    bodyNoScroll: document.querySelector('[data-no-scroll]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.bodyNoScroll.classList.toggle('no-scroll');
+  }
+})();
