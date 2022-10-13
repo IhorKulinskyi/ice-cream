6a5987c8c115d5e365bf1adf1508594d9739adad
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2022-10-13 17:22:19 +0200

    buy now modal script fix

--- src/js/by-now-modal.js
+++ src/js/by-now-modal.js
@@ -12,6 +12,7 @@
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMobileModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
