737291403651a435155269427b2fdc9f234a987b
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2022-10-13 18:17:22 +0200

    no scroll modal + bag fixes

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-franchise-open]'),
     closeModalBtn: document.querySelector('[franchise-data-modal-close]'),
     modal: document.querySelector('[data-modal-franchise]'),
+    bodyNoScroll: document.querySelector('[data-no-scroll]')
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.bodyNoScroll.classList.toggle('no-scroll');
   }
 })();
